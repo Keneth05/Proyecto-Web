c55805c3c5e55fb45eb58b046c26f6bde7053d2a
Keneth05 <keneth05lc@gmail.com>
2024-10-27 23:51:54 -0600

    Avances contacto

--- SRC/Docs/JavaScrip.js
+++ SRC/Docs/JavaScrip.js
@@ -11,3 +11,20 @@ function toggleDescription(event) {
         button.textContent = "Ver Más"; 
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const faqs = document.querySelectorAll(".faq");
+
+    faqs.forEach((faq) => {
+        faq.addEventListener("click", () => {
+            faq.classList.toggle("active");
+
+            const answer = faq.querySelector(".Question-answer");
+            if (faq.classList.contains("active")) {
+                answer.style.maxHeight = answer.scrollHeight + "px";
+            } else {
+                answer.style.maxHeight = 0;
+            }
+        });
+    });
+});
