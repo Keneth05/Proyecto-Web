0fb9e59cda19d938adb73c723a481d9ecaf10ec5
Keneth05 <keneth05lc@gmail.com>
2024-11-08 01:49:21 -0600

    almost finisf  products page

--- SRC/Docs/JavaScrip.js
+++ SRC/Docs/JavaScrip.js
@@ -45,3 +45,18 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function(){
     M.AutoInit();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const botonUp = document.querySelector(".BotonUP");
+    const productsTitle = document.getElementById("ProductsTitle");
+    
+    window.addEventListener("scroll", function() {
+        const rect = productsTitle.getBoundingClientRect(); 
+        
+        if (rect.top >= 0 || (rect.top < window.innerHeight && rect.bottom > 0)) {
+            botonUp.classList.add("hidden");
+        } else {
+            botonUp.classList.remove("hidden"); 
+        }
+    });
+});
